5ccb114b48b89d8f3c98d1715656397f9572a1c6
Aemielvin Loremia <aemielvin.loremia@gmail.com>
2021-10-10 09:24:47 +0800

    Implement crawling of omnicell

--- wikicrawler/src/lib/crawl-types/omnicell/list.js
+++ wikicrawler/src/lib/crawl-types/omnicell/list.js
@@ -1,7 +1,9 @@
 const listUrl = 'https://dauntless.fandom.com/wiki/Omnicells';
 
 async function getUrls () {
-  return [];
+  const url = document.location.href;
+  const headlines = document.querySelectorAll('h2 span.mw-headline');
+  return Array.from(headlines).map(x => `${url}#${x.id}`);
 }
 
 module.exports = {
--- wikicrawler/src/lib/crawl-types/omnicell/pageView.js
+++ wikicrawler/src/lib/crawl-types/omnicell/pageView.js
@@ -1,5 +1,21 @@
 async function extract () {
-  // TODO
+  const url = document.location.href;
+  const headerID = url.match(/([^#]*)$/)[0];
+  const header = document.querySelector(`#${headerID}`).parentElement;
+  const name = header.innerText;
+  
+  const passiveParagraph = header.nextElementSibling;
+  const passive = passiveParagraph.innerText.match(/:(.*)/)[1].trim();
+  
+  const activeParagraph = passiveParagraph.nextElementSibling;
+  const active = activeParagraph.innerText.match(/:(.*)/)[1].trim();
+  
+  return {
+    url,
+    name,
+    active,
+    passive
+  }
 }
 
 module.exports = { extract };
