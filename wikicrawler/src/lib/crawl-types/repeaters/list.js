6ed469ab0bb29dd5a2a918893b0bf865baa407d9
Aemielvin Loremia <aemielvin.loremia@gmail.com>
2021-10-10 10:58:09 +0800

    Implement crawlers for repeaters

--- wikicrawler/src/lib/crawl-types/repeaters/list.js
+++ wikicrawler/src/lib/crawl-types/repeaters/list.js
@@ -1,7 +1,21 @@
 const listUrl = 'https://dauntless.fandom.com/wiki/Ostian_Repeaters';
 
 async function getUrls () {
-  return [];
+  const urls = [];
+  const weaponTables = document.querySelectorAll('table.wikitable.sortable');
+
+  for (const table of Array.from(weaponTables).slice(0, -2)) {
+    const headerLabels = Array.from(table.querySelectorAll('th')).map(x => x.innerText);
+    const nameIndex = headerLabels.findIndex(header => header.toLowerCase() == 'name');
+    const [, ...dataRows] = table.querySelectorAll('tr');
+
+    for (const row of dataRows) {
+      const nameCell = row.querySelectorAll('td')[nameIndex];
+      urls.push(nameCell.querySelector('a').href);
+    }
+  }
+  
+  return urls;
 }
 
 module.exports = {
