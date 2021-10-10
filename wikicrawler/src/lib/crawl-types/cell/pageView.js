3dfd248d7954b62a43f1b40562a153b058fdd762
Aemielvin Loremia <aemielvin.loremia@gmail.com>
2021-10-10 08:40:38 +0800

    Implement crawling for cells

--- wikicrawler/src/lib/crawl-types/cell/pageView.js
+++ wikicrawler/src/lib/crawl-types/cell/pageView.js
@@ -1,5 +1,37 @@
 async function extract () {
-  // TODO
+  const name = document.querySelector('h1').innerText;
+  const url = document.location.href;
+  
+  const infobox = document.querySelector('table.infoboxtable');
+  const infoMap = {};
+  
+  for (const infoRow of infobox.querySelectorAll('tr')) {
+    const labelCell = infoRow.querySelector('td.infoboxlabel');
+    const valueCell = infoRow.querySelector('td div.infoboxdetails');
+
+    if (!labelCell) {
+      continue;
+    }
+
+    const label = labelCell.innerText.toLowerCase().trim();
+
+    if (valueCell) {
+      infoMap[label] = valueCell.innerText.toLowerCase().trim();
+    }
+    else {
+      infoMap[label] = true;
+    }
+  }
+
+  const family = infoMap.family;
+  const perk = name.toLowerCase();
+
+  return {
+    url,
+    name,
+    perk,
+    family
+  };
 }
 
 module.exports = { extract };
